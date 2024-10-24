86b3568e7170852e85fae6a5bf68756de97c2c29
nagydavid89 <davidnagy89@freemail.hu>
2024-10-24 17:33:43 +0200

    localStorageSaved

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
 const light = document.getElementById("light");
 const dark = document.getElementById("dark");
+const savedTheme = localStorage.getItem("theme" || "dark");
 
 const themeSwitch = (theme) => {
 	if (theme === "light") {
+		localStorage.setItem("theme", "light");
 		light.style.display = "none";
 		dark.style.display = "block";
 		document.documentElement.classList.add("dark-mode");
 		return;
 	}
+	localStorage.setItem("theme", "dark");
 	light.style.display = "block";
 	dark.style.display = "none";
 	document.documentElement.classList.remove("dark-mode");
 };
+themeSwitch(savedTheme);
 
 light.addEventListener("click", () => themeSwitch("light"));
 dark.addEventListener("click", () => themeSwitch("dark"));
